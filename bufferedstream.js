fca59feff953f1d6fd04d6e3f6c72588c1bcfbeb
Michael Jackson <mjijackson@gmail.com>
2012-09-09 14:30:57 -0700

    Use 2 space indent

--- bufferedstream.js
+++ bufferedstream.js
@@ -1,5 +1,5 @@
-var util = require("util"),
-    Stream = require("stream").Stream;
+var util = require("util");
+var Stream = require("stream").Stream;
 
 module.exports = BufferedStream;
 
@@ -16,38 +16,38 @@ module.exports = BufferedStream;
  * contents of this stream and passed to `end`.
  */
 function BufferedStream(maxSize, source, sourceEncoding) {
-    Stream.call(this);
+  Stream.call(this);
 
-    // Public interface.
-    this.size = 0;
-    this.encoding = null;
-    this.readable = true;
-    this.writable = true;
-    this.ended = false;
+  // Public interface.
+  this.size = 0;
+  this.encoding = null;
+  this.readable = true;
+  this.writable = true;
+  this.ended = false;
 
-    if (typeof maxSize != "number") {
-        sourceEncoding = source;
-        source = maxSize;
-        maxSize = -1;
+  if (typeof maxSize != "number") {
+    sourceEncoding = source;
+    source = maxSize;
+    maxSize = -1;
+  }
+
+  // This is a soft limit. It's only used to indicate to other streams
+  // writing to this one when they should pause. If negative (the default),
+  // this stream will buffer indefinitely.
+  this.maxSize = maxSize;
+
+  this._buffer = [];
+  this._wait = false;
+  this._flushing = false;
+  this._wasFull = false;
+
+  if (typeof source != "undefined") {
+    if (source instanceof Stream) {
+      source.pipe(this);
+    } else {
+      this.end(source, sourceEncoding);
     }
-
-    // This is a soft limit. It's only used to indicate to other streams
-    // writing to this one when they should pause. If negative (the default),
-    // this stream will buffer indefinitely.
-    this.maxSize = maxSize;
-
-    this._buffer = [];
-    this._wait = false;
-    this._flushing = false;
-    this._wasFull = false;
-
-    if (typeof source != "undefined") {
-        if (source instanceof Stream) {
-            source.pipe(this);
-        } else {
-            this.end(source, sourceEncoding);
-        }
-    }
+  }
 }
 
 util.inherits(BufferedStream, Stream);
@@ -55,82 +55,82 @@ util.inherits(BufferedStream, Stream);
 /**
  * A read-only property that returns `true` if this stream has no data to emit.
  */
-BufferedStream.prototype.__defineGetter__("empty", function empty() {
-    return this._buffer.length == 0;
+BufferedStream.prototype.__defineGetter__("empty", function () {
+  return this._buffer.length == 0;
 });
 
 /**
  * A read-only property that returns `true` if this stream's buffer is full.
  */
-BufferedStream.prototype.__defineGetter__("full", function full() {
-    return this.maxSize >= 0 && this.maxSize < this.size;
+BufferedStream.prototype.__defineGetter__("full", function () {
+  return this.maxSize >= 0 && this.maxSize < this.size;
 });
 
 /**
  * Sets this stream's encoding. If an encoding is set, this stream will emit
  * strings using that encoding. Otherwise, it emits buffers.
  */
-BufferedStream.prototype.setEncoding = function setEncoding(encoding) {
-    this.encoding = encoding;
-}
+BufferedStream.prototype.setEncoding = function (encoding) {
+  this.encoding = encoding;
+};
 
 /**
  * Prevents this stream from emitting `data` events until `resume` is called.
  * This does not prevent writes to this stream.
  */
-BufferedStream.prototype.pause = function pause() {
-    this._wait = true;
-    this.emit("pause");
-}
+BufferedStream.prototype.pause = function () {
+  this._wait = true;
+  this.emit("pause");
+};
 
 /**
  * Resumes emitting `data` events.
  */
-BufferedStream.prototype.resume = function resume() {
-    this._wait = false;
-    this.emit("resume");
-}
+BufferedStream.prototype.resume = function () {
+  this._wait = false;
+  this.emit("resume");
+};
 
 /**
  * Writes the given `chunk` of data to this stream. Returns `false` if this
  * stream is full and should not be written to further until drained, `true`
  * otherwise.
  */
-BufferedStream.prototype.write = function write(chunk, encoding) {
-    if (!this.writable || this.ended) {
-        throw new Error("Stream is not writable");
-    }
+BufferedStream.prototype.write = function (chunk, encoding) {
+  if (!this.writable || this.ended) {
+    throw new Error("Stream is not writable");
+  }
 
-    if (typeof chunk == "string") {
-        chunk = new Buffer(chunk, encoding);
-    }
+  if (typeof chunk == "string") {
+    chunk = new Buffer(chunk, encoding);
+  }
 
-    this._buffer.push(chunk);
-    this.size += chunk.length;
+  this._buffer.push(chunk);
+  this.size += chunk.length;
 
-    if (!this._flushing) {
-        var self = this;
+  if (!this._flushing) {
+    var self = this;
 
-        process.nextTick(function tick() {
-            self.flush();
+    process.nextTick(function tick() {
+      self.flush();
 
-            if (self.empty) {
-                self._flushing = false;
-            } else {
-                process.nextTick(tick);
-            }
-        });
+      if (self.empty) {
+        self._flushing = false;
+      } else {
+        process.nextTick(tick);
+      }
+    });
 
-        this._flushing = true;
-    }
+    this._flushing = true;
+  }
 
-    if (this.full) {
-        this._wasFull = true;
-        return false;
-    }
+  if (this.full) {
+    this._wasFull = true;
+    return false;
+  }
 
-    return true;
-}
+  return true;
+};
 
 /**
  * Tries to emit all data that is currently in the buffer out to all `data`
@@ -139,61 +139,61 @@ BufferedStream.prototype.write = function write(chunk, encoding) {
  * a write and any space is available in the buffer after flushing, a `drain`
  * event is emitted.
  */
-BufferedStream.prototype.flush = function flush() {
-    var chunk;
-    while (!this._wait && this.readable && this._buffer.length) {
-        chunk = this._buffer.shift();
-        this.size -= chunk.length;
+BufferedStream.prototype.flush = function () {
+  var chunk;
+  while (!this._wait && this.readable && this._buffer.length) {
+    chunk = this._buffer.shift();
+    this.size -= chunk.length;
 
-        if (this.encoding) {
-            this.emit("data", chunk.toString(this.encoding));
-        } else {
-            this.emit("data", chunk);
-        }
+    if (this.encoding) {
+      this.emit("data", chunk.toString(this.encoding));
+    } else {
+      this.emit("data", chunk);
     }
+  }
 
-    // Emit "drain" if the stream was full at one point but now
-    // has some room in the buffer.
-    if (this._wasFull && !this.full) {
-        this._wasFull = false;
-        this.emit("drain");
-    }
-}
+  // Emit "drain" if the stream was full at one point but now
+  // has some room in the buffer.
+  if (this._wasFull && !this.full) {
+    this._wasFull = false;
+    this.emit("drain");
+  }
+};
 
 /**
  * Writes the given `chunk` to this stream and queues the `end` event to be
  * called as soon as all `data` events have been emitted.
  */
-BufferedStream.prototype.end = function end(chunk, encoding) {
-    if (this.ended) {
-        throw new Error("Stream is already ended");
+BufferedStream.prototype.end = function (chunk, encoding) {
+  if (this.ended) {
+    throw new Error("Stream is already ended");
+  }
+
+  if (arguments.length > 0) {
+    this.write(chunk, encoding);
+  }
+
+  this.ended = true;
+
+  var self = this;
+
+  process.nextTick(function tick() {
+    if (self.empty) {
+      self.destroy();
+      self.emit("end");
+    } else {
+      process.nextTick(tick);
     }
-
-    if (arguments.length > 0) {
-        this.write(chunk, encoding);
-    }
-
-    this.ended = true;
-
-    var self = this;
-
-    process.nextTick(function tick() {
-        if (self.empty) {
-            self.destroy();
-            self.emit("end");
-        } else {
-            process.nextTick(tick);
-        }
-    });
-}
+  });
+};
 
 /**
  * Destroys this stream immediately. It is no longer readable or writable. This
  * method should rarely ever be called directly by users as it will be called
  * automatically when using BufferedStream#end.
  */
-BufferedStream.prototype.destroy = function destroy() {
-    this._buffer = [];
-    this.readable = false;
-    this.writable = false;
-}
+BufferedStream.prototype.destroy = function () {
+  this._buffer = [];
+  this.readable = false;
+  this.writable = false;
+};
