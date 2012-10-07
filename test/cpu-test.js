0093ffe89affb83d8b57669887065ef65c771af6
Michael Jackson <mjijackson@gmail.com>
2012-10-07 16:07:22 -0700

    Remove CPU test

--- test/cpu-test.js
+++
@@ -1,8 +0,0 @@
-var BufferedStream = require('../buffered-stream');
-
-var stream;
-for (var i = 0; i < 1000; ++i) {
-  stream = new BufferedStream('Hello world');
-  stream.pause();
-  console.log(i + ' stream paused');
-}
